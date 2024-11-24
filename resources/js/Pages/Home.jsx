1b27b0602a894fa1db4d749412d85db2a2d1f095
Andrii <109757085+vo1dwoken@users.noreply.github.com>
2024-11-24 17:54:52 +0100

    Update Home.jsx

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -17,6 +17,27 @@ const Home = () => {
                     View Products
                 </Link>
             </div>
+            {/* Login and Register Links */}
+            <div className="mt-8">
+                <Link
+                    href={route('login')}
+                    className="inline-block px-4 py-2 bg-blue-500 text-white rounded-md text-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                    Log in
+                </Link>
+                <Link
+                    href={route('register')}
+                    className="inline-block ml-4 px-4 py-2 bg-green-500 text-white rounded-md text-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+                >
+                    Register
+                </Link>
+            </div>
+            <p>This is the home page of your computer parts store.</p>
+            <Link href="/products" className="inline-block px-6 py-3 bg-yellow-500 text-white rounded-md text-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500">
+                View Products
+            </Link>
+        </div>
+
             <div className="mt-8 flex justify-center">
                 <Dropdown>
                     <Dropdown.Trigger>
